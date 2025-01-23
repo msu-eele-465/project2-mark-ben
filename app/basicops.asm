73057a5351d280ba81a67d3778a0a55de3691ebd
Mark-H-W <kk7byr@wildmile.com>
2025-01-23 09:45:03 -0700

    add some basic i2c operations

---
+++ app/basicops.asm
@@ -0,0 +1,37 @@
+            .text                           ; Assemble to Flash memory
+            .retain                         ; Ensure current section gets linked
+            .retainrefs
+
+
+i2c_start:
+            bis.b   #BIT4,&P2OUT            ; Bring data line low
+            call    #i2c_delay
+            ret
+
+i2c_stop:   
+            bic.b   #BIT5,&P2OUT            ; Bring clock low
+            call    #i2c_delay
+            bic.b   #BIT4,&P2OUT            ; Bring data low if not already
+            call    #i2c_delay
+            bis.b   #BIT5,&P2OUT            ; Bring clock back high
+            call    #i2c_delay
+            bis.b   #BIT4,&P2OUT            ; Bring data high to signify stop
+            call    #i2_delay
+            ret
+
+
+i2c_tx_bit:
+            bic.b   #BIT5,&P2OUT            ; Bring clock low
+            call    #i2c_delay
+
+            cmp     #0,R15                  ; Check data passed
+            jz      data_zero
+            bis.b   #BIT4,&P2OUT            ; Set data high if passed 1
+            jmp     data_one
+data_zero   bic.b   #BIT4,&P2OUT            ; Set data low if passed 0
+data_one    call    #i2c_delay
+            
+            bis.b   #BIT5,&P2OUT            ; Bring clock back high
+            call    #i2c_delay
+            call    #i2c_delay
+            ret
--- docs/plan.md
+++ docs/plan.md
@@ -5,6 +5,7 @@
 - [ ] i2c_stop
 - [ ] i2c_tx_ack
 - [ ] i2c_rx_ack
+- [ ] i2c_tx_bit
 - [ ] i2c_tx_byte
 - [ ] i2c_delay, delay 25 us
 - [ ] i2c_send_address
@@ -12,3 +13,7 @@
 ## Pins
  - P2.5 - SCLK
  - P2.4 - SDA
+
+## Registers
+ - R14 Delay
+ - R15 Function calls
